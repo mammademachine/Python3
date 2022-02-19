50c02b40c672f08d21e0722aae969add6cc744c0
mammademachine <miletics@yandex.com>
2022-02-19 13:49:44 +0100

    Day 1 Python3

---
+++ 10-mad-libs-game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+something = input("Enter a something: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(something + " are blue")
+print("I love " + celebrity)
---
+++ 4-variable.py
@@ -0,0 +1,4 @@
+char_name = "John"
+char_age  = "70"
+
+print ("There was a man named " + char_name + "he was " + char_age + "old")
